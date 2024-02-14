4871f23bf9615ba47ced2faeaf32e16ff119ed7d
FirelightFlagboy <30697622+FirelightFlagboy@users.noreply.github.com>
2024-02-14 19:26:05 +0100

    Add option to set sentry DSN and environment (#5)
    Fix #4

--- home-manager-module.nix
+++ home-manager-module.nix
@@ -1,8 +1,6 @@
 self: { lib, pkgs, config, ... }:
 
 let
-  cfgClient = config.programs.parsec-cloud-client;
-  clientFinalPackage = cfgClient.package;
   clientDefaultPackage = self.packages.${pkgs.stdenv.hostPlatform.system}.parsec-cloud-client;
 in
 {
@@ -61,31 +59,63 @@ in
       example = true;
       description = "Display first launch's welcome message";
     };
-  };
 
-  config = lib.mkIf cfgClient.enable {
-    home.packages = [ clientFinalPackage ];
-
-    xdg.configFile."parsec/config.json".text = builtins.toJSON {
-      telemetry_enabled = cfgClient.telemetry;
-      gui_language = cfgClient.lang;
-      preferred_org_creation_backend_addr = cfgClient.preferredServer;
-      data_base_dir = cfgClient.dataBaseDir;
-      gui_check_version_at_startup = cfgClient.checkForUpdate;
+    sentryDsn = mkOption {
+      type = types.str;
+      default = "https://863e60bbef39406896d2b7a5dbd491bb@o155936.ingest.sentry.io/1212848";
+      example = "https://foobar.ingest.my-sentry-dsn.com/random_id";
+      description = "Sentry DSN";
     };
 
-    # TODO: Add icon
-    xdg.dataFile."applications/parsec-cloud.desktop".text = ''
-      [Desktop Entry]
-      Name=Parsec
-      Comment=Secure cloud framework
-      Exec=${clientFinalPackage} core gui %u
-      Terminal=false
-      Type=Application
-      Categories=Network;FileTransfer;Security;
-      StartupNotify=false
-      StartupWMClass=Parsec
-      MimeType=x-scheme-handler/parsec
-    '';
+    sentryEnvironment = mkOption {
+      type = types.str;
+      default = "production";
+      example = "staging";
+      description = "Sentry environment";
+    };
   };
+
+  config =
+    let
+      cfgClient = config.programs.parsec-cloud-client;
+      clientFinalPackage = cfgClient.package;
+      client = pkgs.symlinkJoin {
+        name = "parsec-cloud-client";
+        paths = [ clientFinalPackage ];
+        buildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+          set -x
+          echo out=$out
+          ls -lR $out
+          wrapProgram "$out/bin/parsec" \
+            --set PARSEC_SENTRY_DSN ${cfgClient.sentryDsn} \
+            --set PARSEC_SENTRY_ENVIRONMENT ${cfgClient.sentryEnvironment}
+        '';
+      };
+    in
+    lib.mkIf cfgClient.enable {
+      home.packages = [ client ];
+
+      xdg.configFile."parsec/config.json".text = builtins.toJSON {
+        telemetry_enabled = cfgClient.telemetry;
+        gui_language = cfgClient.lang;
+        preferred_org_creation_backend_addr = cfgClient.preferredServer;
+        data_base_dir = cfgClient.dataBaseDir;
+        gui_check_version_at_startup = cfgClient.checkForUpdate;
+      };
+
+      # TODO: Add icon
+      xdg.dataFile."applications/parsec-cloud.desktop".text = ''
+        [Desktop Entry]
+        Name=Parsec
+        Comment=Secure cloud framework
+        Exec=${client} core gui %u
+        Terminal=false
+        Type=Application
+        Categories=Network;FileTransfer;Security;
+        StartupNotify=false
+        StartupWMClass=Parsec
+        MimeType=x-scheme-handler/parsec
+      '';
+    };
 }
