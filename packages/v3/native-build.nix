1263a57faa412eb37ada3fdf3e21d1581b2028d2
Firelight Flagboy <firelight.flagboy@gmail.com>
2025-02-12 10:16:49 +0100

    fix(client): native build require heavy patching
    - Like for the update script, we need to patch the dependencies `xlsx`
      to use the CDN. That could be simplified if using `importNpmLock`.
    - Require patched `npmConfigHook` to set the correct `nodeSrc` dir for
      `node-gyp` to be able to find `config.gypi` (would be fixed by
      nixos/nixpkgs#381409).
    - Build the `canvas` dependencies require system dependencies (`pixman`,
      `cairo`, `pango`).

--- packages/v3/native-build.nix
+++ packages/v3/native-build.nix
@@ -1,10 +1,50 @@
-{ pkgs, src, version, isPrerelease, ... }:
+{
+  pkgs,
+  src,
+  version,
+  isPrerelease,
+  ...
+}:
 
-pkgs.buildNpmPackage {
+let
+  patchedSrc = pkgs.stdenvNoCC.mkDerivation {
+    inherit version src;
+    pname = "parsec-cloud-client-src";
+    patches = [
+      ./patches/use-cdn-instead-of-vendored-xlsx.patch
+    ];
+    patchFlags = "--strip=1 --verbose";
+    installPhase = # shell
+      ''
+        mkdir -p "$out"
+        cp -ra client "$out"
+      '';
+  };
+  inherit (pkgs) buildNpmPackage;
+  nodejs = pkgs.nodejs_20;
+  # TODO: Should be fixed once https://github.com/NixOS/nixpkgs/pull/381409 is merged.
+  npmConfigHook = pkgs.makeSetupHook {
+    name = "npm-config-hook";
+    substitutions = {
+      nodeSrc = nodejs;
+      nodeGyp = "${nodejs}/lib/node_modules/npm/node_modules/node-gyp/bin/node-gyp.js";
+
+      # Specify `diff`, `jq`, and `prefetch-npm-deps` by abspath to ensure that the user's build
+      # inputs do not cause us to find the wrong binaries.
+      diff = "${pkgs.diffutils}/bin/diff";
+      jq = "${pkgs.jq}/bin/jq";
+      prefetchNpmDeps = "${pkgs.prefetch-npm-deps}/bin/prefetch-npm-deps";
+
+      nodeVersion = nodejs.version;
+      nodeVersionMajor = pkgs.lib.versions.major nodejs.version;
+    };
+  } ./npm-config-hook.sh;
+in
+buildNpmPackage {
   inherit version;
   pname = "parsec-native-build";
 
-  src = "${src}/client";
+  src = "${patchedSrc}/client";
 
   npmDepsHash = "sha256-N2dVEcaXhpbUJfzLRy1RADIPJyP46RIgh/VNBppLy9w=";
 
@@ -21,13 +61,23 @@ pkgs.buildNpmPackage {
         -e 's;node ./scripts/vite_build_for_native.cjs;${buildCmd};' \
         -i package.json
     '';
+  npmConfigHook = npmConfigHook;
 
-  env =
-    {
-      PLATFORM = "native";
-      CYPRESS_INSTALL_BINARY = "0";
-    };
-
+  env = {
+    PLATFORM = "native";
+    CYPRESS_INSTALL_BINARY = "0";
+  };
+  nativeBuildInputs = [
+    pkgs.pkg-config
+  ];
+  nodejs = nodejs;
+  buildInputs = [
+    # Dependencies for canvas dependency
+    pkgs.pixman
+    pkgs.cairo.dev
+    pkgs.pango.dev
+    # End of dependencies for canvas dependency
+  ];
   npmBuildScript = "native:build";
 
   installPhase = ''
@@ -35,12 +85,16 @@ pkgs.buildNpmPackage {
     cp -rva dist/{index.html,assets} $out
   '';
 
-  meta = let inherit (pkgs.lib) majorMinor licenses platforms; in {
-    homepage = "https://parsec.cloud/";
-    description = "Parsec cloud native client build used for the electron app";
-    branch = "releases/${majorMinor version}";
-    license = [ licenses.bsl11 ];
-    changelog = "https://github.com/Scille/parsec-cloud/tree/v${version}/HISTORY.rst";
-    platforms = platforms.all;
-  };
+  meta =
+    let
+      inherit (pkgs.lib) majorMinor licenses platforms;
+    in
+    {
+      homepage = "https://parsec.cloud/";
+      description = "Parsec cloud native client build used for the electron app";
+      branch = "releases/${majorMinor version}";
+      license = [ licenses.bsl11 ];
+      changelog = "https://github.com/Scille/parsec-cloud/tree/v${version}/HISTORY.rst";
+      platforms = platforms.all;
+    };
 }
