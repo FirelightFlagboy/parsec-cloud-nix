77f2ee1dcb71bd1ce53a6ddd9a8e2d15eb8a4d30
Firelight Flagboy <firelight.flagboy@gmail.com>
2026-01-09 10:01:21 +0100

    fixup! refactor(updater): Now working with new package scope

--- scripts/update-parsec-v3.sh
+++ scripts/update-parsec-v3.sh
@@ -7,6 +7,7 @@ ROOTDIR=${ROOTDIR:=$(realpath -s "$SCRIPTDIR/..")}
 UPDATE_MODE=${1:-stable}
 
 nix run nixpkgs#nix-update -- --flake --version="$UPDATE_MODE" parsec-cloud.v3.src
+URL=$(nix eval .#parsec-cloud.v3.src.src.url --raw)
 
 function fail {
     local lc="$BASH_COMMAND" previous_rc=$?
@@ -18,21 +19,5 @@ function fail {
 
 trap "fail" ERR
 
-SOURCE=$(nix build .#parsec-cloud.v3.src --no-link --print-out-paths)
-
-CLIENT_NPM_DEPS_HASH=$(nix run nixpkgs#prefetch-npm-deps "$SOURCE"/client/package-lock.json)
-ELECTRON_NPM_DEPS_HASH=$(nix run nixpkgs#prefetch-npm-deps "$SOURCE"/client/electron/package-lock.json)
-
-echo "Updating Npm dependencies hash"
-sed -i \
-    -e "s;npmDepsHash = \".*\";npmDepsHash = \"${CLIENT_NPM_DEPS_HASH}\";" \
-    $ROOTDIR/packages/v3/native-build.nix
-
-sed -i \
-    -e "s;npmDepsHash = \".*\";npmDepsHash = \"${ELECTRON_NPM_DEPS_HASH}\";" \
-    $ROOTDIR/packages/v3/electron-app.nix
-
-if [ "$(git status --porcelain | grep -e packages/v3/source.nix -e packages/v3/native-build.nix -e packages/v3/electron-app.nix --count)" -ne 3 ]; then
-    echo "Invalid number of file changes, a 'sed' command has failed"
-    exit 1
-fi
+nix run nixpkgs#nix-update -- --flake --no-src --url="$URL" parsec-cloud.v3.client
+nix run nixpkgs#nix-update -- --flake --no-src --url="$URL" parsec-cloud.v3.cli
